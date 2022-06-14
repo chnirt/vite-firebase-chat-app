4d75b3e45749000bbe044d87d20e1520b2b13587
chnirt <trinhchinchin@gmail.com>
2022-06-14 17:57:59 +0700

    Update: hotfix

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
   useLayoutEffect(() => {
     const unsubscribed = refreshRemote();
     const isTurnServer = getRemoteValue('vite_app_turn_server', 'boolean')
-    console.log('vite_app_turn_server', vite_app_turn_server)
+    console.log('vite_app_turn_server', isTurnServer)
     setEnableTurnServer(isTurnServer)
     return () => {
       unsubscribed;
