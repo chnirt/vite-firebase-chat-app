d56b50c600a6ac1a6292b60087c5357a317d7820
chnirt <trinhchinchin@gmail.com>
2022-07-06 01:03:58 +0700

    Update: remove forkme

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RemoteConfigProvider>
-      <ForkMe />
+      {/* <ForkMe /> */}
       <AuthProvider>
         <Router basename={basename}>
           <App />
