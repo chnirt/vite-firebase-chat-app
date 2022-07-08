5b85a5a8971ccb7e1b59a031c42359537fa099e7
chnirt <trinhchinchin@gmail.com>
2022-07-08 16:40:58 +0700

    Update: rebuild

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
-import Helmet from 'react-helmet'
 
 import App from './App'
 import { AuthProvider, RemoteConfigProvider, I18nProvider } from './context'
