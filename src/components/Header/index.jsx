58a077293a7199fa658db1d4635f4c4f2433c73f
chnirt <trinhchinchin@gmail.com>
2022-07-08 16:38:00 +0700

    Update: add header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+import { APP_NAME } from '../../env'
+
+export const Header = () => {
+  return (
+    <Helmet>
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1"
+      />
+      <title>{APP_NAME} Messenger</title>
+    </Helmet>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+import Helmet from 'react-helmet'
 
 import App from './App'
 import { AuthProvider, RemoteConfigProvider, I18nProvider } from './context'
 // import { ForkMe } from './components'
+import { Header } from './components'
 import { basename } from './constants'
 
 import './index.less'
@@ -12,6 +14,7 @@ import './index.less'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RemoteConfigProvider>
+      <Header />
       {/* <ForkMe /> */}
       <AuthProvider>
         <Router basename={basename}>
